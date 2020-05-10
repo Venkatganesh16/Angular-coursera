edc1d056569621281eae0b8c226797c453bc4456
Venkat Ganesh <neelavenkatganesh16@gmail.com>
2020-05-10 20:43:15 +0530

    single page part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES} from '../shared/dishes';
+import { Promotion } from '../shared/promotion';
+import { PROMOTIONS } from '../shared/promotions';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,13 @@ export class DishService {
 
   getDishes(): Dish[] {
     return DISHES;
+  }
+
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
   }
 }
